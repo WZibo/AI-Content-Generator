b447b982c38b0905e873f53d57e0ce0f7e70e446
ZIBO <wangzibo@bu.edu>
2024-10-07 21:53:51 -0400

    Update connectDB.js

--- Backend/utils/connectDB.js
+++ Backend/utils/connectDB.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(
-      "mongodb+srv://wzb0615:P1Ly506I3WukVK42@ai-generator.ku0z6.mongodb.net/ai-generator?retryWrites=true&w=majority&appName=AI-Generator"
+      ""
     );
 
     console.log(`Mongodb connected ${conn.connection.host}`);
